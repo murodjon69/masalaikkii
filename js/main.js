73110442378959121877cc47f98baede9dd761f2
murodjon69 <mxakimmirzaev@gmail.com>
2022-01-11 15:59:16 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var array =[102,24,201,333,34 ];
+var largest=0;
+array.forEach((element)=>{
+ if(largest<element){
+   largest=element
+ }
+});
+console.log (largest); 
+
+var array =[550,22,200,100,344 ];
+var largest=0;
+array.forEach((element)=>{
+ if(largest<element){
+   largest=element
+ }
+});
+console.log (largest); 
